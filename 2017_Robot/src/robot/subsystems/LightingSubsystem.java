eb7b79938df44a45b63ccd1ea9274b46c3009f00
Quinlan McNellen <riku@Luka>
2017-03-08 17:48:16 -0500

    RGB lighting control

---
+++ 2017_Robot/src/robot/subsystems/LightingSubsystem.java
@@ -0,0 +1,52 @@
+package robot.subsystems;
+
+import com.toronto.subsystems.T_Subsystem;
+import com.mindsensors.CANLight;
+
+import robot.RobotMap;
+import robot.commands.LightingCommand;
+
+/**
+ * @author riku
+ *
+ */
+public class LightingSubsystem extends T_Subsystem {
+	
+	CANLight frameLights;
+	/* (non-Javadoc)
+	 * @see com.toronto.subsystems.T_Subsystem#robotInit()
+	 */
+	public void initDefaultCommand() {
+		setDefaultCommand(new LightingCommand());
+	}
+	
+	public void robotInit() {
+		// TODO Auto-generated method stub
+		frameLights = new CANLight(RobotMap.CANLIGHT_CAN_ADDRESS);
+	}
+	
+	public void setBlueAlliance() {
+		frameLights.showRGB(0, 0, 255);
+	}
+	
+	public void setRedAlliance() {
+		frameLights.showRGB(255, 0, 0);
+	}
+	
+	public void setGreen() {
+		frameLights.showRGB(0, 255, 0);
+	}
+	
+	public void setTeal() {
+		frameLights.showRGB(0, 221, 192);
+	}
+	/* (non-Javadoc)
+	 * @see com.toronto.subsystems.T_Subsystem#updatePeriodic()
+	 */
+	@Override
+	public void updatePeriodic() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
